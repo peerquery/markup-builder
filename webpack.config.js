ac10134cf3acfc5d1d5e75c09a9aef993a34f684
dzivenu <makafuigdzivenu@gmail.com>
2018-10-15 07:36:32 +0000

    uninstall babel deps and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './lib/index.js',
@@ -13,17 +12,6 @@ module.exports = {
         // https://github.com/webpack/webpack/issues/6677
         // https://github.com/webpack/webpack/issues/6642
         globalObject: 'typeof self !== \'undefined\' ? self : this',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                use: 'babel-loader',
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()]
     },
     mode: 'production',
 };
